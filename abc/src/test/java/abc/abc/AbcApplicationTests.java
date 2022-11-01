778199ed4ac68ef32086903c5fa935b8a6e9fb5a
KirinBeast <106136535+KirinBeast@users.noreply.github.com>
2022-11-01 21:37:56 +0800

    Update

---
+++ abc/src/test/java/abc/abc/AbcApplicationTests.java
@@ -0,0 +1,13 @@
+package abc.abc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
